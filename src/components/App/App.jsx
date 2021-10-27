f393e1a440453f3100dfba126ca18bd2c44ac104
Adam Johnson <adam.p.johnson.dc@gmail.com>
2021-10-27 13:47:21 -0700

    installed and configured CSS modules, added stylesheets and directories for existing components, configured gitignore

---
+++ src/components/App/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import ProductDetail from '../ProductDetail/ProductDetail.jsx';
+// import QandA from '../QuestionsAnsAnswers/QuestionsAndAnswers.jsx';
+// import RandOC from '../RelatedItemsAndOutfitCreation/RelatedItemsAndOutfitCreation.jsx';
+// import RatAndRev from '../RatingsAndReviews/RatingsAndReviews.jsx';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <h1>Hello from App</h1>
+    )
+  }
+}
+
+export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
   entry: path.join(__dirname, "src", "index.js"),
   output: {
-    path:path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
-    publicPath: "/"
+    publicPath: "/",
   },
   module: {
     rules: [
@@ -15,10 +15,23 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
         },
       },
-    ]
-  }
-}
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
