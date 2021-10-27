9e3d509bdc888a7bd565cebacb531e7077f47ba6
Sara Schaeffer <sara.schaeffer617@gmail.com>
2021-10-27 09:09:27 -0700

    initial repo setup with src and dist directories

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import ProductDetail from './ProductDetail.jsx';
+// import QandA from './QuestionsAndAnswers.jsx';
+// import RandOC from './RelatedItemsAndOutfitCreation.jsx';
+// import RatAndRev from './RatingsAndReviews.jsx';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <h1>Hello from App</h1>
+    )
+  }
+}
+
+export default App;
