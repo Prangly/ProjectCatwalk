4cb44509aeabe8632890de668d06ef1a85d8967b
Sara Schaeffer <sara.schaeffer617@gmail.com>
2021-10-28 07:49:28 -0700

    implemented RatAndRev component and rendered in app, adjusted minor ESLint catches

---
+++ src/components/RatingsAndReviews/RatingsAndReviews.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './styles.css';
+
+const RatAndRev = () => (
+  <div id={styles.ratingsAndReviews}>
+    <h1>Ratings and Reviews</h1>
+  </div>
+);
+
+export default RatAndRev;
