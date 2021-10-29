4d659d7be2506052dc6b3db15b4221d8d1a63f94
Adam Johnson <adam.p.johnson.dc@gmail.com>
2021-10-29 10:22:07 -0700

    rendered sample data to DetailsText and StylesAndSizes

--- src/components/ProductDetail/DetailText/DetailText.jsx
+++ src/components/ProductDetail/DetailText/DetailText.jsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import ProductFeature from './ProductFeature/ProductFeature';
 
-export default function DetailText() {
+export default function DetailText({ product }) {
+  const features = product.features.map(
+    // eslint-disable-next-line comma-dangle
+    (feature) => <ProductFeature key={feature.feature} feature={feature} />
+  );
+
   return (
     <div data-testid="detailText">
-      Detail Text
+      <div id="productSlogan">
+        {product.slogan}
+      </div>
+      <div id="productDescription">
+        {product.description}
+      </div>
+      <div id="productFeatures">
+        {features}
+      </div>
     </div>
   );
 }
+
+DetailText.propTypes = {
+  product: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    slogan: PropTypes.string,
+    description: PropTypes.string,
+    category: PropTypes.string,
+    default_price: PropTypes.string,
+    created_at: PropTypes.string,
+    updated_at: PropTypes.string,
+    features: PropTypes.arrayOf(PropTypes.object),
+  }).isRequired,
+};
---
+++ src/components/ProductDetail/DetailText/DetailText.test.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {
+  render, screen,
+} from '@testing-library/react';
+
+import '@testing-library/jest-dom';
+import DetailText from './DetailText';
+import sampleProduct from '../SampleData/SampleProduct';
+
+describe('Detail Text tests', () => {
+  it('should render the product slogan on the page', () => {
+    render(<DetailText product={sampleProduct} />);
+    expect(screen.getByText('Blend in to your crowd')).toBeInTheDocument();
+  });
+});
