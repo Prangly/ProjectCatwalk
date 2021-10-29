a4bec068e8cafa49e968282e2afee5259a9f5dbd
Yoseob Kim <ykim9611@gmail.com>
2021-10-29 15:57:42 -0700

    Implemented search ipnut box and button. Added tests

--- src/components/QuestionsAndAnswers/QuestionsSearchInput/QuestionsSearchInput.jsx
+++ src/components/QuestionsAndAnswers/QuestionsSearchInput/QuestionsSearchInput.jsx
@@ -8,7 +8,7 @@ const QuestionsSearchInput = (props) => {
 
   return (
     <div>
-      <input type="text" placeholder="Have a question? Search for answers..." className={styles.searchInput} onChange={() => changeInput(event.target.value)} />
+      <input data-testid="questionsSearchInput" type="text" placeholder="Have a question? Search for answers..." className={styles.searchInput} onChange={() => changeInput(event.target.value)} />
       <input type="button" value="Search" onClick={() => setInput('Submitted')} />
     </div>
   );
