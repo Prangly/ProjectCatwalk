53f7d67580bcee30d827c1c721c2e61b206cce60
Abe Froman <abefroman@Abes-MacBook-Pro.local>
2021-10-29 11:39:08 -0700

    Creates two new children components -- RelatedItems and Outfit, and renders them to the browser

---
+++ src/components/RelatedItemsAndOutfitCreation/RelatedItems/RelatedItems.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function RelatedItems() {
+  return (
+    <div data-testid="relatedItems">
+      RelatedItems
+    </div>
+  );
+};
--- src/components/RelatedItemsAndOutfitCreation/RelatedItemsAndOutfitCreation.jsx
+++ src/components/RelatedItemsAndOutfitCreation/RelatedItemsAndOutfitCreation.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import styles from './styles.css';
+import RelatedItems from './RelatedItems/RelatedItems';
+import Outfit from './Outfit/Outfit';
 
 const RandOC = () => (
-  <div id={styles.relatedItems}>
+  <div data-testid="rAndOC" id={styles.relatedItems}>
     <h1>Related Items and Outfit Creation</h1>
+    <RelatedItems />
+    <Outfit />
   </div>
 );
 
