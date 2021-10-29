3ffa5464da39c569632778839b3914ad2e13d103
Adam Johnson <adam.p.johnson.dc@gmail.com>
2021-10-28 17:55:26 -0700

    added sub-componenets to ProductDetails and added resolve property to webpackconfig for import statements

--- src/components/ProductDetail/ProductDetail.test.jsx
+++ src/components/ProductDetail/ProductDetail.test.jsx
@@ -12,4 +12,19 @@ describe('Product Detail tests', () => {
     render(<ProductDetail />);
     expect(screen.getByTestId('productDetail')).toBeInTheDocument();
   });
+
+  it('should contain an "imageCarousel" component', () => {
+    render(<ProductDetail />);
+    expect(screen.getByTestId('imageCarousel')).toBeInTheDocument();
+  });
+
+  it('should contain a "stylesAndSizes" component', () => {
+    render(<ProductDetail />);
+    expect(screen.getByTestId('stylesAndSizes')).toBeInTheDocument();
+  });
+
+  it('should have a "detailText" component', () => {
+    render(<ProductDetail />);
+    expect(screen.getByTestId('detailText')).toBeInTheDocument();
+  });
 });
---
+++ src/components/ProductDetail/StylesAndSizes/StylesAndSizes.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function StylesAndSizes() {
+  return (
+    <div data-testid="stylesAndSizes">
+      Styles and Sizes
+    </div>
+  );
+}
