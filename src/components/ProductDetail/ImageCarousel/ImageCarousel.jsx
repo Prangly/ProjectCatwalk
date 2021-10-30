4d597f96ce2ecbe0cd4c889c2c834638b5480128
Adam Johnson <adam.p.johnson.dc@gmail.com>
2021-10-29 18:00:53 -0700

    implemented basic carousel functionality with static data

--- src/components/ProductDetail/ImageCarousel/ImageCarousel.jsx
+++ src/components/ProductDetail/ImageCarousel/ImageCarousel.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styles from './styles.css';
 
 export default function ImageCarousel({ product }) {
   const sampleStyle = product.results[0];
-
+  const { name } = sampleStyle;
   const urls = sampleStyle.photos.map((result) => result.url);
+  const [currentImage, setCurrentImage] = useState(0);
+
+  const onIncrement = (direction) => {
+    if (direction === 'up' && currentImage < urls.length - 1) {
+      setCurrentImage(currentImage + 1);
+    } else if (direction === 'down' && currentImage > 0) {
+      setCurrentImage(currentImage - 1);
+    }
+  };
 
   return (
     <div id={styles.imageCarousel} data-testid="imageCarousel">
-      <img className={styles.bigImage} alt={sampleStyle.name} src={urls[0]} />
+      <button type="button" onClick={() => onIncrement('down')} onKeyDown={() => onIncrement('down')} id={styles.leftBar} className={styles.scrollBar}> &lt;</button>
+
+      <img className={styles.bigImage} alt={name} src={urls[currentImage]} />
+
+      <button type="button" onClick={() => onIncrement('up')} onKeyDown={() => onIncrement('up')} id={styles.rightBar} className={styles.scrollBar}> &gt; </button>
+
     </div>
   );
 }
