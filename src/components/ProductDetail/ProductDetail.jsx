3a884f15eb5f39f0b20be8833e8450ec94153510
Abe Froman <abefroman@Abes-MacBook-Pro.local>
2021-11-01 16:57:51 -0700

    Syncing my version

--- src/components/ProductDetail/ProductDetail.jsx
+++ src/components/ProductDetail/ProductDetail.jsx
@@ -1,26 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './styles.css';
 import ImageCarousel from './ImageCarousel/ImageCarousel';
 import StylesAndSizes from './StylesAndSizes/StylesAndSizes';
 import DetailText from './DetailText/DetailText';
-import SampleProduct from './SampleData/SampleProduct';
-import sampleStyles from './SampleData/SampleStyles';
+import SampleProduct from '../../SampleData/SampleProduct';
+import sampleStyles from '../../SampleData/SampleStyles';
 
 const currentProduct = SampleProduct;
 
-const ProductDetail = () => (
-  <div data-testid="productDetail" id={styles.productDetail}>
-    <div id={styles.upperContainer}>
-      <ImageCarousel id={styles.imageCarousel} product={sampleStyles} />
-      <StylesAndSizes
-        id={styles.stylesAndSizes}
-        productStyles={sampleStyles}
-        name={SampleProduct.name}
-      />
-    </div>
-    <DetailText id={styles.detailText} product={currentProduct} />
-  </div>
+const ProductDetail = () => {
+  const [currentStyle, setCurrentStyle] = useState(0);
 
-);
+  return (
+    <div data-testid="productDetail" id={styles.productDetail}>
+      <div id={styles.upperContainer}>
+        <ImageCarousel
+          id={styles.imageCarousel}
+          productStyles={sampleStyles}
+          currentStyle={currentStyle}
+        />
+        <StylesAndSizes
+          id={styles.stylesAndSizes}
+          productStyles={sampleStyles}
+          name={SampleProduct.name}
+          setCurrentStyle={setCurrentStyle}
+        />
+      </div>
+      <DetailText id={styles.detailText} product={currentProduct} />
+    </div>
+  );
+};
 
 export default ProductDetail;
--- src/components/RatingsAndReviews/RatingsAndReviews.jsx
+++ src/components/RatingsAndReviews/RatingsAndReviews.jsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './styles.css';
 import WriteAReview from './WriteAReview/WriteAReview';
+import SampleReviews from '../../SampleData/SampleReviews';
+import ReviewsList from './ReviewsList/ReviewsList';
 
-const RatAndRev = () => (
-  <div data-testid="ratAndRev" id={styles.ratingsAndReviews}>
-    <h1>Ratings and Reviews Parent Component</h1>
-    <WriteAReview />
-  </div>
-);
+const RatAndRev = () => {
+  const [currentRev, setCurrentRev] = useState(0);
+
+  return (
+    <div data-testid="ratAndRev" id={styles.ratingsAndReviews}>
+      <h1>Ratings and Reviews</h1>
+      <ReviewsList
+        currentRev={currentRev}
+        reviews={SampleReviews}
+      />
+      <WriteAReview />
+    </div>
+  );
+};
 
 export default RatAndRev;
