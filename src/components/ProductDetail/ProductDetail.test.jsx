10e5af030da1849441095db9f0e98b5602b0e764
Adam Johnson <adam.p.johnson.dc@gmail.com>
2021-11-01 18:45:47 -0700

    fixed tests

--- src/components/ProductDetail/ProductDetail.test.jsx
+++ src/components/ProductDetail/ProductDetail.test.jsx
@@ -10,32 +10,32 @@ import '@testing-library/jest-dom';
 import ProductDetail from './ProductDetail';
 import sampleProduct from '../../SampleData/SampleProduct'
 import { act } from 'react-dom/test-utils';
-describe('Product Detail tests', async () => {
-  it('should recognize react testing library methods', () => {
+describe('Product Detail tests', () => {
+  it('should recognize react testing library methods', async () => {
     render(<ProductDetail currentProduct={sampleProduct} />);
     await expect(screen.getByTestId('productDetail')).toBeInTheDocument();
     expect(screen.getByTestId('productDetail')).toBeInTheDocument();
   });
 
-  it('should contain an "imageCarousel" component', () => {
+  it('should contain an "imageCarousel" component', async () => {
     render(<ProductDetail currentProduct={sampleProduct} />);
     await expect(screen.getByTestId('imageCarousel')).toBeInTheDocument();
     expect(screen.getByTestId('imageCarousel')).toBeInTheDocument();
   });
 
-  it('should contain a "stylesAndSizes" component', () => {
+  it('should contain a "stylesAndSizes" component', async () => {
     render(<ProductDetail currentProduct={sampleProduct} />);
     await expect(screen.getByTestId('stylesAndSizes')).toBeInTheDocument();
     expect(screen.getByTestId('stylesAndSizes')).toBeInTheDocument();
   });
 
-  it('should have a "detailText" component', () => {
+  it('should have a "detailText" component', async () => {
     render(<ProductDetail currentProduct={sampleProduct} />);
     await expect(screen.getByTestId('detailText')).toBeInTheDocument();
     expect(screen.getByTestId('detailText')).toBeInTheDocument();
   });
 
-  it('should change the style on icon click', () => {
+  it('should change the style on icon click', async () => {
     render(<ProductDetail currentProduct={sampleProduct}
     />);
     const image = screen.getByTestId('carouselImage')
