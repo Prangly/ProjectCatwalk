94f2d12401121169a338c26794962765e3f37173
Sara Schaeffer <sara.schaeffer617@gmail.com>
2021-11-04 13:52:44 -0700

    Minor changes to correct integration tests

--- src/components/RatingsAndReviews/RatingsAndReviews.jsx
+++ src/components/RatingsAndReviews/RatingsAndReviews.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import styles from './styles.css';
 import WriteAReview from './WriteAReview/WriteAReview';
 import SampleReviews from '../../SampleData/SampleReviews';
