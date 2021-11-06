0b19b75de38541ebb8e0c6cfa0a181d8914b88ca
Sara Schaeffer <sara.schaeffer617@gmail.com>
2021-11-06 10:21:50 -0700

    added all necessary pieces of review without styling

--- src/components/RatingsAndReviews/ReviewsList/Review.jsx
+++ src/components/RatingsAndReviews/ReviewsList/Review.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from '../styles.css';
 
 const Review = ({ review }) => (
-  <div data-testid="review">
+  <div data-testid="review" id={styles.review}>
     {review.rating}
+    {review.date}
     {review.summary}
+    {review.body}
+    {review.recommend}
+    {review.reviewer_name}
+    {review.response}
+    {review.helpfulness}
   </div>
 );
 
 Review.propTypes = {
   review: PropTypes.shape({
     rating: PropTypes.number,
+    date: PropTypes.instanceOf(Date),
     summary: PropTypes.string,
+    body: PropTypes.string,
+    recommend: PropTypes.bool,
+    reviewer_name: PropTypes.string,
+    response: PropTypes.string,
+    helpfulness: PropTypes.number,
   }).isRequired,
 };
 
