413d291751d3fea586f3b7bdb0be196788567ec1
Yoseob Kim <juliehwang@Julias-MacBook-Pro.local>
2021-11-08 11:08:59 -0800

    Deleted Search Submit button.

--- src/components/QuestionsAndAnswers/QuestionsSearchInput/QuestionsSearchInput.jsx
+++ src/components/QuestionsAndAnswers/QuestionsSearchInput/QuestionsSearchInput.jsx
@@ -3,13 +3,11 @@ import React, { useState } from 'react';
 import styles from './styles.css';
 
 const QuestionsSearchInput = (props) => {
-  const [input, changeInput] = useState();
-  const [submit, setInput] = useState();
+  const [searchInput, changeSearchInput] = useState();
 
   return (
     <div>
-      <input data-testid="questionsSearchInput" type="text" placeholder="Have a question? Search for answers..." className={styles.searchInput} onChange={(event) => changeInput(event.target.value)} />
-      <input type="button" value="Search" className={styles.searchButton} onClick={() => setInput('Submitted')} />
+      <input data-testid="questionsSearchInput" type="text" placeholder="Have a question? Search for answers..." className={styles.searchInput} onChange={(event) => changeSearchInput(event.target.value)} />
     </div>
   );
 };
