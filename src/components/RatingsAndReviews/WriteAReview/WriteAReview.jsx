37099f454c1a8eb8e983fba5c7c881af84d0341c
Sara Schaeffer <sara.schaeffer617@gmail.com>
2021-11-08 11:49:09 -0800

    added star rating to write review component rather than reviews list

--- src/components/RatingsAndReviews/WriteAReview/WriteAReview.jsx
+++ src/components/RatingsAndReviews/WriteAReview/WriteAReview.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Modal from 'react-modal';
 import axios from 'axios';
+import StarRating from '../StarRating/StarRating';
 
 const WriteAReview = ({ openModal, setOpenModal, currentProductId }) => {
   const postReview = () => {
@@ -13,6 +14,7 @@ const WriteAReview = ({ openModal, setOpenModal, currentProductId }) => {
     <Modal isOpen={openModal} onRequestClose={() => setOpenModal(false)}>
       <div>
         <h2>Add Your Review</h2>
+        <StarRating />
         <textarea type="text" cols="100" rows="10" maxLength="1000"></textarea>
       </div>
     </Modal>
