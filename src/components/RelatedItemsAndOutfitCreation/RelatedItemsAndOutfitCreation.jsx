8f0b24bae814f2d97ab44c66d4dc8c8d4aa87b6e
Abe Froman <abefroman@Abes-MacBook-Pro.local>
2021-11-09 16:49:02 -0800

    Removed the words 'Related Items and Outfit' from the display

--- src/components/RelatedItemsAndOutfitCreation/RelatedItemsAndOutfitCreation.jsx
+++ src/components/RelatedItemsAndOutfitCreation/RelatedItemsAndOutfitCreation.jsx
@@ -7,7 +7,6 @@ import Outfit from './Outfit/Outfit';
 // eslint-disable-next-line react/prop-types
 const RandOC = ({ currentProduct, setCurrentProductID, currentOutfit, removeFromOutfit }) => (
   <div data-testid="rAndOC" id={styles.rAndOC}>
-    <h1>Related Items and Outfit Creation</h1>
     <RelatedItems currentProduct={currentProduct} setCurrentProductID={setCurrentProductID} />
     <Outfit currentOutfit={currentOutfit} removeFromOutfit={removeFromOutfit} />
   </div>
