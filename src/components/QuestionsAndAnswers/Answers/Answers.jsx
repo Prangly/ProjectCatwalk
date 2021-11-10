9b1c85cf4acc6c11ee922b34d610d81774f7ce74
Yoseob Kim <juliehwang@Julias-MacBook-Pro.local>
2021-11-10 09:40:40 -0800

    Made it so that if the answerer is the seller, the font will be bold

--- src/components/QuestionsAndAnswers/Answers/Answers.jsx
+++ src/components/QuestionsAndAnswers/Answers/Answers.jsx
@@ -44,7 +44,7 @@ const Answers = ({ answer, setAnswerHelpfulness, setReportAnswer }) => {
       <div id={styles.answersAuthor}>
         by
         {' '}
-        {answer.answerer_name}
+        {answer.answerer_name.toLowerCase() === 'seller' ? <span id={styles.seller}>Seller</span> : <span>{answer.answerer_name}</span>}
         ,
         {' '}
         {months[answer.date.slice(5, 7)]}
