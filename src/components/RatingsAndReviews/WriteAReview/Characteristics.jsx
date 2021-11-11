6ef8c4d0a70f7a0fd415a9a6d6a561db3c981d53
Sara Schaeffer <sara.schaeffer617@gmail.com>
2021-11-11 14:22:17 -0800

    basic table with some styling

--- src/components/RatingsAndReviews/WriteAReview/Characteristics.jsx
+++ src/components/RatingsAndReviews/WriteAReview/Characteristics.jsx
@@ -1,10 +1,31 @@
 import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import styles from './styles.css';
 
 const Characteristics = ({ characteristics, setCharacteristics }) => {
   console.log(characteristics);
   return (
-    <h2>This is where characteristics will be entered.</h2>
+    <>
+      <h2>This is where characteristics will be entered.</h2>
+      <table id={styles.charsTable}>
+        <thead id={styles.charsHead}>
+          <td />
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td>4</td>
+          <td>5</td>
+        </thead>
+        <tr>
+          <td />
+          <td>1</td>
+          <td>2</td>
+          <td>3</td>
+          <td>4</td>
+          <td>5</td>
+        </tr>
+      </table>
+    </>
   );
 };
 
