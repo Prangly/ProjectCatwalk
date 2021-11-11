1084a0eedf8a69d8582770d89995e05d0110a141
Sara Schaeffer <sara.schaeffer617@gmail.com>
2021-11-11 14:33:48 -0800

    rendering rows dynamically based on num of characteristics

--- src/components/RatingsAndReviews/WriteAReview/Characteristics.jsx
+++ src/components/RatingsAndReviews/WriteAReview/Characteristics.jsx
@@ -4,6 +4,14 @@ import styles from './styles.css';
 
 const Characteristics = ({ characteristics, setCharacteristics }) => {
   console.log(characteristics);
+  console.log(Object.entries(characteristics));
+  console.log(Object.keys(characteristics));
+  const namesOfChars = Object.keys(characteristics);
+  const namesRows = namesOfChars.map((name) => (
+    <tr>
+      <td>{name}</td>
+    </tr>
+  ));
   return (
     <>
       <h2>This is where characteristics will be entered.</h2>
@@ -16,14 +24,7 @@ const Characteristics = ({ characteristics, setCharacteristics }) => {
           <td>4</td>
           <td>5</td>
         </thead>
-        <tr>
-          <td />
-          <td>1</td>
-          <td>2</td>
-          <td>3</td>
-          <td>4</td>
-          <td>5</td>
-        </tr>
+        {namesRows}
       </table>
     </>
   );
